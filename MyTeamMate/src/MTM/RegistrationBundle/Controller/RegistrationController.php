3ef575b191fbfb94719e156629bec586b71b7975
lootss <lucille.dhaleine@gmail.com>
2013-05-01 23:25:39 +0200

    fix problème d'enregistrement en base

--- MyTeamMate/src/MTM/RegistrationBundle/Controller/RegistrationController.php
+++ MyTeamMate/src/MTM/RegistrationBundle/Controller/RegistrationController.php
@@ -19,10 +19,8 @@ class RegistrationController extends Controller {
 				->add('email', 'email', array('attr' => array('placeholder' => 'Adresse Mail')))
 				->add('password', 'repeated', array('type' => 'password',
   												  'invalid_message' => 'Les mots de passe doivent correspondre',
-   												 'options' => array('required' => true),
-  													 'first_options'  => array('label' => ' ' ),
-  													'second_options' => array('label' => ' ')))		
-				->add('acceptusemail', 'checkbox')
+   												 'options' => array('required' => true)))		
+				->add('acceptusemail', 'checkbox',array('required' => false))
 				->getForm();
 		
 		if ($request->isMethod('POST')) {

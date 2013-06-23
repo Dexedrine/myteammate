e1afb06e87279bba2c3b7937f1b5e4a29dbe1733
anthony <anthony.maia80@gmail.com>
2013-06-23 18:20:15 +0200

    Formulaire star-rating pour les notes

--- MyTeamMate/src/MTM/ProfileBundle/Controller/NoteController.php
+++ MyTeamMate/src/MTM/ProfileBundle/Controller/NoteController.php
@@ -1,71 +1,34 @@
 <?php
 
 namespace MTM\ProfileBundle\Controller;
+use MTM\ProfileBundle\Entity\Note;
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\SecurityContext;
 use Symfony\Component\HttpFoundation\Request;
 use MTM\ProfileBundle\Entity\Profile;
+use MTM\CoreBundle\Entity\TeamMate;
 
 class NoteController extends Controller {
 	
-	public function profileAction() {
+	public function noteProfileAction($id) {
 		$teammate = $this->get('security.context')->getToken()->getUser();
 		if(!$teammate) return $this->redirect($this->generateUrl('login'));
-
-		if (!$teammate->getProfile()) {
-			return $this
-					->render('MTMProfileBundle:Profile:no_profile.html.twig');
-		}
-
-		return $this
-				->render('MTMProfileBundle:Profile:profile.html.twig',
-						array( 'teammate' => $teammate));
-	}
-	
-		public function othersProfileAction($id) {
-		$teammate = $this->get('security.context')->getToken()->getUser();
-		if(!$teammate) return $this->redirect($this->generateUrl('login'));		
 		
 		$em = $this->getDoctrine()->getManager();
 		$repository = $em->getRepository('MTMCoreBundle:TeamMate');
-
+		
 		$profile_teammate = $repository->findOneById($id);
+	 	/*$note = new Note() ;
+	 	
+	 	$form = $this->createFormBuilder($note)
+			 	->add('value', 'text',  array('label' => 'Note'))
+			 	->getForm();*/
 
 		return $this
-				->render('MTMProfileBundle:Profile:others_profile.html.twig',
+				->render('MTMProfileBundle:Note:note_profile.html.twig',
 						array('teammate' => $teammate,
 							'profile_teammate' => $profile_teammate));
-	}
-
-	public function addAction(Request $request) {
-		$teammate = $this->get('security.context')->getToken()->getUser();
-		if(!$teammate) return $this->redirect($this->generateUrl('login'));
-		
-		$profile = new Profile();
-
-
-		$form = $this->createFormBuilder($profile)
-				->add('name', 'text', array('label' => 'Nom'))
-				->add('firstname', 'text', array('label' => 'Prénom'))
-				->add('sexe', 'text', array('label' => 'Sexe(H/F)'))
-				->add('description', 'text',  array('label' => 'Description'))
-				->getForm();
-
-		if ($request->isMethod('POST')) {
-			$form->bind($request);
-
-			if ($form->isValid()) {
-				$em = $this->getDoctrine()->getManager();
-				$teammate->setProfile($profile);
-				$em->persist($profile);
-				$em->flush();
-				return $this->redirect($this->generateUrl('profile'));
-			}
-		}
-
-		return $this
-				->render('MTMProfileBundle:Profile:add_profile.html.twig',
-						array('form' => $form->createView()));
 	}
 }
